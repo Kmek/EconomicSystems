bd7f6a1709ec88f1fbbb38312e7e2f6f7a5fa7d8
Kmek <kmekeefe@gmail.com>
2020-03-05 10:55:25 -0500

    make separate div for pageLayout

--- navigation.js
+++ navigation.js
@@ -2,7 +2,7 @@
 // Creates navigation buttons so they don't have to be copied
 // Horizontal Navigation Version
 
-const navTitles = ["Traditional", "Market", "Command", "Mixed", "MLA"]
+const navTitles = ["Traditional Economy", "Market Economy", "Command Economy", "Mixed Economy", "MLA"]
 
 var navigationDiv = document.getElementById("navigation")
 
